2169682bedf2fc4e784c6ea702548ffe8116b38a
Christof Röhl <userpattern@gmail.com>
2023-12-21 09:59:53 +0100

    solved

--- js/index.js
+++ js/index.js
@@ -12,27 +12,77 @@ We want to add some functionality to the buttons on this page. When pressing a b
 */
 
 redButton.addEventListener("click", () => {
-  // use your functions to
-  // - remove all classes
-  // - then add the class name: "red"
+  function removeAllColors() {
+    box.classList.remove("red");
+    box.classList.remove("green");
+    box.classList.remove("blue");
+    box.classList.remove("gray");
+  }
+  function addColorRed() {
+    box.classList.add("red");
+  }
+  removeAllColors();
+  addColorRed();
 });
 
 blueButton.addEventListener("click", () => {
   // use your functions to
   // - remove all classes
   // - then add the class name: "blue"
+  function removeAllColors() {
+    box.classList.remove("red");
+    box.classList.remove("green");
+    box.classList.remove("blue");
+    box.classList.remove("gray");
+  }
+  function addColorBlue() {
+    box.classList.add("blue");
+  }
+  removeAllColors();
+  addColorBlue();
 });
 
 greenButton.addEventListener("click", () => {
   // use your functions to
   // - remove all classes
   // - then add the class name: "green"
+  function removeAllColors() {
+    box.classList.remove("red");
+    box.classList.remove("green");
+    box.classList.remove("blue");
+    box.classList.remove("gray");
+  }
+  function addColorGreen() {
+    box.classList.add("green");
+  }
+  removeAllColors();
+  addColorGreen();
 });
 
 grayButton.addEventListener("click", () => {
   // use your functions to
   // - remove all classes
   // - then add the class name: "gray"
+  function removeAllColors() {
+    box.classList.remove("red");
+    box.classList.remove("green");
+    box.classList.remove("blue");
+    box.classList.remove("gray");
+  }
+  function addColorGray() {
+    box.classList.add("gray");
+  }
+  removeAllColors();
+  addColorGray();
 });
 
 // Write your two functions below:
+function removeAllColors() {
+  box.classList.remove("red");
+  box.classList.remove("green");
+  box.classList.remove("blue");
+  box.classList.remove("gray");
+}
+function addColorGray() {
+  box.classList.add("gray");
+}
